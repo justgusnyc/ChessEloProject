40ecf56b64609d1df0a4d146d3ec4c5f72a1af71
tuneboks <65516753+tuneboks@users.noreply.github.com>
2022-05-05 19:24:10 +0200

    Update TestProgram.java

--- SEChessFinalProject/src/chess/TestProgram.java
+++ SEChessFinalProject/src/chess/TestProgram.java
@@ -1,19 +1,30 @@
 package chess;
 
 import chess.Players.Bracket;
+import java.util.ArrayList;
 
 public class TestProgram{
     
     public static void main(String[] args) {
         MatchingAlgo MA = new MatchingAlgo();
-        PlayGame play = new PlayGame();
+        ArrayList<Players> players = new ArrayList<Players>();
+        
         Players G = new Players("Gus", 1000);
         Players C = new Players("Cesar", 1800);
         Players V = new Players("Vish", 800);
         Players N = new Players("Noah", 1000);
         Players A = new Players("Arthur", 950);
-        Players D = new Players("David", 2000);
-    
+        Players R = new Players("Renzo", 920);
+        
+        players.add(G);
+        players.add(C);
+        players.add(V);
+        players.add(N);
+        players.add(A);
+        players.add(R);
+        
+        PlayGame play = new PlayGame(players);
+        
         G.getPlayers();
         System.out.println("Buffr");
         C.getPlayers();
@@ -26,8 +37,8 @@ public class TestProgram{
         System.out.println(beg.getValue());
         System.out.println(Bracket.lookup(G.getElo()));
         System.out.println(Bracket.lookup(C.getElo()));
-        System.out.println("Optimal First Match for Gus player: "+ MA.optimalMatch(G));
-        play.beginGame(G);
+//        System.out.println("Optimal First Match for Gus player: "+ MA.optimalMatch(G));
+        play.beginGame();
         V.getPlayers();
 
         
